17296b6861b56aa686f23959c329215f62ba7991
Ridham Lalakiya <ridhamlalakiya@gmail.com>
2026-04-28 14:22:36 +0530

    Update - Ridham Clinic App

--- push.bat
+++ push.bat
@@ -2,9 +2,9 @@
 set PATH=C:\Program Files\Git\bin;%PATH%
 git config user.name "Ridham Lalakiya"
 git config user.email "ridhamlalakiya@gmail.com"
+git rm --cached sqlite.db 2>nul
 git add .
-git commit -m "Initial commit - Ridham Clinic App" --author="Ridham Lalakiya <ridhamlalakiya@gmail.com>"
-git branch -M main
+git commit -m "Update - Ridham Clinic App" --author="Ridham Lalakiya <ridhamlalakiya@gmail.com>"
 git pull origin main --allow-unrelated-histories --strategy-option=theirs
 git push -u origin main
 echo.
